a66c1dc8422ecd6ddd6507bcfa05f496da3b6bf6
AkshayPoriya <akshayporiya1234@gmail.com>
2020-09-28 17:55:59 +0530

    UC4 Repeat Till Winning Point

--- Program.cs
+++ Program.cs
@@ -11,26 +11,35 @@ namespace SnakeAndLadderProblem
             const int Ladder = 2;
             const int Snake = 3;
             int currentPosition = Start_Position;
+            int winningPosition = 100;
             Random randObj = new Random();
+            int noOfMoves = 0;
 
-            int dieNumber = randObj.Next(1, 7);
-            Console.WriteLine("Die Number is {0}",dieNumber);
-            int option = randObj.Next(1, 4);
-            Console.WriteLine("Option is {0}", option);
-            switch (option)
+            while(currentPosition < winningPosition)
             {
-                case No_Play:
-                    break;
-                case Ladder:
-                    currentPosition += dieNumber;
-                    break;
-                case Snake:
-                    currentPosition -= dieNumber;
-                    break;
-                default:
-                    break;
+                int dieNumber = randObj.Next(1, 7);
+                Console.WriteLine("Die Number is {0}", dieNumber);
+                int option = randObj.Next(1, 4);
+                Console.WriteLine("Option is {0}", option);
+                switch (option)
+                {
+                    case No_Play:
+                        break;
+                    case Ladder:
+                        currentPosition += dieNumber;
+                        break;
+                    case Snake:
+                        currentPosition -= dieNumber;
+                        if (currentPosition < Start_Position)
+                            currentPosition = Start_Position;
+                        break;
+                    default:
+                        break;
+                }
+                noOfMoves++;
             }
             Console.WriteLine("Current Position is {0}",currentPosition);
+            Console.WriteLine("Total moves used {0}", noOfMoves);
         }
     }
 }
