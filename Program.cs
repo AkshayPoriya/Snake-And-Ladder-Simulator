02151de4b549be0fac7719000a28a18b5b5776f1
AkshayPoriya <akshayporiya1234@gmail.com>
2020-09-28 17:39:59 +0530

    UC3 Check for Option

--- Program.cs
+++ Program.cs
@@ -7,9 +7,30 @@ namespace SnakeAndLadderProblem
         static void Main(string[] args)
         {
             const int Start_Position = 0;
+            const int No_Play = 1;
+            const int Ladder = 2;
+            const int Snake = 3;
+            int currentPosition = Start_Position;
             Random randObj = new Random();
+
             int dieNumber = randObj.Next(1, 7);
             Console.WriteLine("Die Number is {0}",dieNumber);
+            int option = randObj.Next(1, 4);
+            Console.WriteLine("Option is {0}", option);
+            switch (option)
+            {
+                case No_Play:
+                    break;
+                case Ladder:
+                    currentPosition += dieNumber;
+                    break;
+                case Snake:
+                    currentPosition -= dieNumber;
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine("Current Position is {0}",currentPosition);
         }
     }
 }
